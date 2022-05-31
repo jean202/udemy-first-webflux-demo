07abe04edae20685691bad60fbb7beadbbff457b
jean202 <hb13pud731@gmail.com>
2022-05-31 14:05:19 +0900

    project setting

---
+++ src/main/java/com/jhkim2/udemyfirstwebfluxdemo/UdemyFirstWebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jhkim2.udemyfirstwebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyFirstWebfluxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyFirstWebfluxDemoApplication.class, args);
+    }
+
+}
