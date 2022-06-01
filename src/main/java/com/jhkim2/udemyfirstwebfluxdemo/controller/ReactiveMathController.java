13b8e97e1234d61dc5a8feee03f48b7483805c88
JinHa Kim <jhkim2@iconloop.com>
2022-06-02 08:19:47 +0900

    [controller/service] ReactiveMath start

---
+++ src/main/java/com/jhkim2/udemyfirstwebfluxdemo/controller/ReactiveMathController.java
@@ -0,0 +1,30 @@
+package com.jhkim2.udemyfirstwebfluxdemo.controller;
+
+import com.jhkim2.udemyfirstwebfluxdemo.dto.Response;
+import com.jhkim2.udemyfirstwebfluxdemo.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("reactive-math")
+public class ReactiveMathController {
+
+    @Autowired
+    private ReactiveMathService mathService;
+
+    @GetMapping("square/{input}")
+    public Mono<Response> findSquare(@PathVariable int input) {
+        return this.mathService.findSquare(input);
+    }
+
+    @GetMapping("table/{input}")
+    public Flux<Response> multiplicationTable(@PathVariable int input) {
+        return this.mathService.multiplicationTable(input);
+    }
+
+}
