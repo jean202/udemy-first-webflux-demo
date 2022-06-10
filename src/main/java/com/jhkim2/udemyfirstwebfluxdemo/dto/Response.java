7e5decb827a991702f346d39f79060ecd14a0134
jean202 <hb13pud731@gmail.com>
2022-06-10 18:36:19 +0900

    [config/test] WebClientConfig, Lec01~06Test

--- src/main/java/com/jhkim2/udemyfirstwebfluxdemo/dto/Response.java
+++ src/main/java/com/jhkim2/udemyfirstwebfluxdemo/dto/Response.java
@@ -2,10 +2,12 @@ package com.jhkim2.udemyfirstwebfluxdemo.dto;
 
 import java.util.Date;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
+@NoArgsConstructor
 public class Response {
 
     private Date date = new Date();
---
+++ src/test/java/com/jhkim2/udemyfirstwebfluxdemo/BaseTest.java
@@ -0,0 +1,11 @@
+package com.jhkim2.udemyfirstwebfluxdemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class BaseTest {
+
+
+
+}
